670f9f07d3461071eefdb5bc572a5dc3d5d0d9c1
Suraj Ravindra Dhumal <srdhumal2002@gmail.com>
2024-10-23 22:18:32 +0530

    Add project files.

---
+++ MyCrudApp.ApiEndpoint/Controllers/EmployeesController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyCrudApp.ApiEndpoint.Controllers
+{
+    [Route("[controller]/[action]")]
+    [ApiController]
+    public class EmployeesController : ControllerBase
+    {
+    }
+}
